43b9aa1d230c8ebc560fdea359bca975f0ac8ad8
David Merkel <merkel_david@yahoo.com>
2017-11-15 13:45:33 -0600

    finished clock example

---
+++ clockRadio/clockRadio/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clockRadio
+{
+    class ClockRadio
+    {
+        public bool alarm;
+        public string radioStation;
+
+        public ClockRadio( bool OnOff, string station)
+        {
+            this.alarm = OnOff;
+            this.radioStation = station;
+
+        }
+
+        public TimeSpan getTime ()
+        {
+            return DateTime.Now.TimeOfDay;
+        }
+
+        public void setAlarm(bool OnOff)
+        {
+            alarm = OnOff;
+        }
+
+        public void setStation(string station)
+        {
+            radioStation = station;
+        }
+
+        public void getStatus()
+        {
+            if (alarm == true) {
+                Console.WriteLine("The alarm is on\n the station is"+ radioStation +" and the time is"+ DateTime.Now.TimeOfDay);
+            }
+            else
+            {
+                Console.WriteLine("The alarm is off \n The station is " + radioStation + "\n The time is " + DateTime.Now.TimeOfDay);
+
+            }
+        }
+    }
+}
---
+++ clockRadio/clockRadio/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clockRadio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program main = new Program();
+
+            Console.WriteLine("Welcome to your basic alarm set up");
+
+            //bool alarm = main.OnOrOff(main.getAlarmStatus());
+
+            ClockRadio radio = new ClockRadio(main.OnOrOff(main.getAlarmStatus()), main.getRadioStation() );
+
+            radio.getStatus();
+
+            Console.ReadKey();
+        }
+
+        public string getRadioStation()
+        {
+            Console.WriteLine("What station would you like to set your alarm to?");
+
+            return Console.ReadLine();
+
+        }
+        public string getAlarmStatus()
+        {
+            Console.WriteLine("Would you like to set your alarm to on or off?");
+
+            return Console.ReadLine();
+        }
+        public bool OnOrOff(string input)
+        {
+            if(input == "on")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
